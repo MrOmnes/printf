4d9941f6250137c1f161184981c90048a1d7168f
Secatricia <3560@holbertonschool.com>
2021-11-08 09:32:53 +0000

    Modification

--- main.h
+++ main.h
@@ -4,23 +4,22 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int _printf(const char *format, ...);
-void print_char(va_list listFormat);
-void print_integer(va_list listFormat);
-void print_float(va_list listFormat);
-void print_string(va_list listFormat);
-
-
 /**
  * struct searchtype - Struct searchtype
  *
  * @typeOf: The type we search
  * @f: The function associated
  */
-typedef struct whattype
+typedef struct search_type
 {
-	char *a;
+	char *type;
 	void (*f)(va_list);
-} choice;
+} search_type_t;
+
+int _printf(const char *format, ...);
+void print_char(va_list args);
+void print_integer(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
 
 #endif
