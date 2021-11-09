572ae8ddcd228c7f93eb47f4dc89eb72af64209f
MrOmnes <3644@holbertonschool.com>
2021-11-09 16:00:55 +0000

    Fix betty

--- printf.c
+++ printf.c
@@ -3,56 +3,56 @@
 #include "main.h"
 #include <unistd.h>
 
-int _printf(const char *format, ...)
+void (*search_type (char format))(va_list)
 {
-	search_type_t format_of_char[] = {/*structure qui choisit la fonction à appeler*/
-		{"c", print_char},
-		{"i", print_integer},
-		{"d", print_integer},
-		{"f", print_float},
-		{"s", print_char_pointer},
-		{"b", print_binary},
-		{"u", print_unsigned},
-		{"o", print_octal},
-		{"x", print_hexadecimal},
-	  {NULL, NULL}};
-	int i = 0;
-	va_list args; /*declare une liste d'argument*/
+	int loop = 0;
 
+	search_type_t format_of_char[] = {/*get function to call*/
+	{"c", print_char},
+	{"i", print_integer},
+	{"d", print_integer},
+	{"f", print_float},
+	{"s", print_char_pointer},
+	{"b", print_binary},
+	{"u", print_unsigned},
+	{"o", print_octal},
+	{"x", print_hexadecimal},
+	{NULL, NULL}};
+
+	while (format_of_char[loop].type != NULL)
+	{
+		if (format_of_char[loop].type[0] == format)
+		{
+			return(format_of_char[loop].f);
+		}
+		loop++;
+	}
+	_putchar('%');
+	_putchar(format);
+	return (NULL);
+}
+
+int _printf(const char * const format, ...)
+{
+	int i = 0;
+
+	va_list args; /*declare une liste d'argument*/
 	va_start(args, format); /*initialise args*/
 
-	for (i = 0; format[i] >= 32 && format[i] <= 126 && format[i]; i++) /*Est ce que format[i] est affichable*/
+	for (i = 0; format[i] >= 32 && format[i] <= 126 && format[i]; i++)
 	{
-		if (format[i] != 37) /*si format[i] different %*/
+
+		if (format[i] == 37)
 		{
+			if (format[i + 1] == 37) /*%*/
+				_putchar('%'), i++;
+			else
+				search_type(format[i + 1])(args), i+=2;
+		}
+		if (format[i] != 37)
 			_putchar(format[i]);
-		}
-		else
-		{
-			if (format[i] == 37)		  /*si format[i] = %*/
-			{							  /*Quel est le type? integrer, char, string..*/
-				if (format[i + 1] == 105) /*i*/
-					format_of_char[1].f(args), i++;
-				if (format[i + 1] == 100) /*d*/
-					format_of_char[2].f(args), i++;
-				if (format[i + 1] == 99) /*c*/
-					format_of_char[0].f(args), i++;
-				if (format[i + 1] == 115) /*s*/
-					format_of_char[4].f(args), i++;
-				if (format[i + 1] == 98) /*b*/
-					format_of_char[5].f(args), i++;
-				if (format[i + 1] == 117) /*u*/
-					format_of_char[6].f(args), i++;
-				if (format[i + 1] == 111) /*o*/
-					format_of_char[7].f(args), i++;
-				if (format[i + 1] == 120) /*x*/
-					format_of_char[8].f(args), i++;
-				if (format[i + 1] == 37) /*%*/
-					_putchar('%'), i++;
-			}
-		}
 	}
 	va_end(args);
-	printf("\n");
+	_putchar('\n');
 	return (_strlen(format)); /*return la taille de format*/
 }
