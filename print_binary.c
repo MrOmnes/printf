0ce031ff182a05c9f8036fb7d9b34260ade6d129
Secatricia <3560@holbertonschool.com>
2021-11-09 13:40:03 +0000

    Ajout betty fichier print_binary.c et print_octal.c

--- print_binary.c
+++ print_binary.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
+/**
+ * print_binary - fonction qui affiche en binaire
+ * @args: argument
+ */
 void print_binary(va_list args)
 {
-   int num = va_arg(args, int);
-   int i = 0;
-   char s[90];
-   for (i = 0; num > 0; i++)
-   {
-    s[i] = num % 2;
-	num = num / 2;
-   }
-	for (i = i -1; i >= 0; i--)
-		    _putchar(s[i] + 48);
+	int num = va_arg(args, int);
+	int i = 0;
+	char s[90];
+
+	for (i = 0; num > 0; i++)
+	{
+		s[i] = num % 2;
+		num = num / 2;
+	}
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i] + 48);
 }
--- print_octal.c
+++ print_octal.c
@@ -1,15 +1,21 @@
 #include "main.h"
 
+/**
+ * print_octal - fonction qui affiche en base 8
+ * @args: argument
+ */
+
 void print_octal(va_list args)
 {
-   unsigned int num = va_arg(args, unsigned int);
-   int i = 0;
-   char s[90];
-   for (i = 0; num > 0; i++)
-   {
-    s[i] = num % 8;
-	num = num / 8;
-   }
-	for (i = i -1; i >= 0; i--)
-		    _putchar(s[i] + 48);
+	unsigned int num = va_arg(args, unsigned int);
+	int i = 0;
+	char s[90];
+
+	for (i = 0; num > 0; i++)
+	{
+		s[i] = num % 8;
+		num = num / 8;
+	}
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i] + 48);
 }
