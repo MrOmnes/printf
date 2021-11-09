1ffc01a65671d85c0d566a6c87982a8226eb072f
MrOmnes <3644@holbertonschool.com>
2021-11-09 18:41:49 +0000

    Try

--- printf.c
+++ printf.c
@@ -52,7 +52,7 @@ int _printf(const char * const format, ...)
 
 	va_start(args, format); /*initialise args*/
 
-	if (format[i])
+	if (!format[i])
 		return (0);
 
 	for (i = 0; format[i] >= 32 && format[i] <= 126 && format[i]; i++)
