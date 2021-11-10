e2508ddd4519eec1ae116dc98ce0277704595ffa
MrOmnes <3644@holbertonschool.com>
2021-11-10 16:57:41 +0000

    Improve reverse

--- print_reverse.c
+++ print_reverse.c
@@ -10,7 +10,13 @@ int print_reverse(va_list args)
 	char *str = va_arg(args, char *);
 	int c = _strlen(str);
 
-	while (c >= 0)
+	if (str == NULL)
+	{
+		str = "(null)";
+		return (-1);
+	}
+
+	while (c >= 0 && str != NULL)
 	{
 		_putchar(str[c]);
 		c--;
