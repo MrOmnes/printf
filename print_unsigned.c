e84f39b8bd6ab1b891cb0fdfc6593bf8637c9b09
MrOmnes <3644@holbertonschool.com>
2021-11-10 17:47:35 +0000

    Fix return of print_unsigned

--- print_unsigned.c
+++ print_unsigned.c
@@ -10,7 +10,6 @@ int print_unsigned(va_list args) /*Fonction qui print l'argument de type int*/
 {
 	int i = va_arg(args, int);
 
-	print_number_unsigned_int(i);
+	return (print_number_unsigned_int(i));
 
-	return (0);
 }
